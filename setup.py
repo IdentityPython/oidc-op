a44b1ba555f555f9a2ebed73ed6dc5be5578de1b
roland <roland@catalogix.se>
2019-02-14 14:22:04 +0100

    Changed dependencies.

--- setup.py
+++ setup.py
@@ -58,9 +58,9 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        'cryptojwt>=0.3.1'
-        'oidcmsg>=0.3.5',
-        "oidcendpoint>=0.4.6",
+        'cryptojwt>=0.6.5'
+        'oidcmsg>=0.6.2',
+        "oidcendpoint>=0.5.1",
         "requests",
         "cherrypy",
     ],
