e3a2d54e717a63b19c52c81cfe74713bcdd08adb
roland <roland@catalogix.se>
2019-09-06 11:11:32 +0200

    Removed special version requirement on pyyaml.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        "PyYaml>=18.1",
+        "PyYaml",
         "cryptojwt>=0.7.9",
         'oidcmsg>=0.6.3',
         'oidcendpoint>=0.8.4'
