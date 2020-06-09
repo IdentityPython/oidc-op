fde24cbc0887da94ad2aae28067e0dab79c25e9b
roland <roland@catalogix.se>
2020-06-09 13:52:08 +0200

    Updated version and oidcendpoint version dependency.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
         "pyyaml",
-        'oidcendpoint>=0.13.4'
+        'oidcendpoint>=1.0.0'
     ],
     zip_safe=False,
     cmdclass={'test': PyTest},
