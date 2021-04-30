f82637ccfa936512b0f52859a912675dc69dc8e1
Roland Hedberg <roland@catalogix.se>
2021-04-30 09:39:07 +0200

    userinfo setup should not be done here.

--- src/oidcop/endpoint_context.py
+++ src/oidcop/endpoint_context.py
@@ -214,7 +214,7 @@ class EndpointContext(OidcContext):
             if _func:
                 _func()
 
-        for item in ["userinfo", "login_hint_lookup", "login_hint2acrs"]:
+        for item in ["login_hint_lookup", "login_hint2acrs"]:
             _func = getattr(self, "do_{}".format(item), None)
             if _func:
                 _func()
