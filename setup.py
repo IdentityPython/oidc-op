2da7071e5cd8743cc21cedf53323210c8af3920d
peppelinux <giuseppe.demarco@unical.it>
2021-06-12 14:13:58 +0200

    v2.0.1

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        "oidcmsg==1.3.2",
+        "oidcmsg==1.3.3",
         "cryptojwt==1.5.2",
         "pyyaml",
         "jinja2>=2.11.3",
