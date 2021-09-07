bcab6b01ff850903eb51049c3710640d282847a7
Roland Hedberg <roland@catalogix.se>
2021-09-07 13:44:00 +0200

    Fixed cookie_info(). Added tests and fixed a test result.

--- src/oidcop/user_authn/user.py
+++ src/oidcop/user_authn/user.py
@@ -102,17 +102,14 @@ class UserAuthnMethod(object):
 
     def cookie_info(self, cookie: List[dict], client_id: str) -> dict:
         _context = self.server_get("endpoint_context")
-        if cookie and "value" in cookie[0]:
-            vals = cookie
-        else:
-            try:
-                logger.debug("parse_cookie@UserAuthnMethod")
-                vals = _context.cookie_handler.parse_cookie(
-                    cookies=cookie, name=_context.cookie_handler.name["session"]
-                )
-            except (InvalidCookieSign, AssertionError, AttributeError) as err:
-                logger.warning(err)
-                vals = None
+        try:
+            logger.debug("parse_cookie@UserAuthnMethod")
+            vals = _context.cookie_handler.parse_cookie(
+                cookies=cookie, name=_context.cookie_handler.name["session"]
+            )
+        except (InvalidCookieSign, AssertionError, AttributeError) as err:
+            logger.warning(err)
+            vals = []
 
         logger.debug("Value cookies: {}".format(vals))
 
