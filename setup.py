a2e76e3453297d356edff22699a9ee21e0ccce1a
Roland Hedberg <roland@catalogix.se>
2021-11-17 08:41:01 +0100

    Use latest oidcmsg. Don't specify cryptojwt requirement.

--- setup.py
+++ setup.py
@@ -72,8 +72,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        "oidcmsg==1.5.0",
-        "cryptojwt==1.5.2",
+        "oidcmsg==1.5.1",
         "pyyaml",
         "jinja2>=2.11.3",
         "responses>=0.13.0"
