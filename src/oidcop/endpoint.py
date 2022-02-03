fe3cc63f5d67b654f79858916006456af2a4b404
Nikos Sklikas <nsklikas@admin.grnet.gr>
2022-02-03 11:51:23 +0200

    Add more clear client/token authn exceptions

--- src/oidcop/endpoint.py
+++ src/oidcop/endpoint.py
@@ -91,6 +91,7 @@ class Endpoint(object):
     response_placement = "body"
     client_authn_method = ""
     default_capabilities = None
+    get_client_id_from_token = None
 
     def __init__(self, server_get: Callable, **kwargs):
         self.server_get = server_get
@@ -175,7 +176,6 @@ class Endpoint(object):
             req = self.request_cls()
 
         # Verify that the client is allowed to do this
-        _client_id = ""
         auth_info = self.client_authentication(req, http_info, endpoint=self, **kwargs)
 
         if "client_id" in auth_info:
@@ -205,14 +205,6 @@ class Endpoint(object):
         return self.do_post_parse_request(
             request=req, client_id=_client_id, http_info=http_info, **kwargs
         )
-
-    def get_client_id_from_token(
-        self,
-        endpoint_context: EndpointContext,
-        token: str,
-        request: Optional[Union[Message, dict]] = None,
-    ):
-        return ""
 
     def client_authentication(self, request: Message, http_info: Optional[dict] = None, **kwargs):
         """
