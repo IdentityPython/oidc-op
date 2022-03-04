d5dd35300c8b5b4b5998155eb52a27c187acfabf
0xMattijs <mattijs@zerocopter.com>
2022-03-04 08:20:24 +0100

    Isolate changes

--- example/fastapi/main.py
+++ example/fastapi/main.py
@@ -1,42 +1,35 @@
 import json
 import logging
-import os
-import uvicorn
 
 from fastapi import Depends
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi.logger import logger
 from fastapi.openapi.models import Response
-from utils import service_endpoint
 from models import AuthorizationRequest
 from models import WebFingerRequest
 from utils import verify
 
 from oidcop.exception import FailedAuthentication
 from oidcop.server import Server
-from oidcmsg.configure import create_from_config_file
-from oidcmsg.configure import Configuration
-from oidcop.configure import OPConfiguration
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-config_file = 'config.json'
-
-logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
-config = create_from_config_file(Configuration,
-    entity_conf=[{
-        "class": OPConfiguration, "attr": "op",
-        "path": ["op", "server_info"]
-    }],
-    filename=config_file,
-    base_path=dir_path)
-app.server = Server(config.op, cwd="/oidc")
+app.server = None
+
 
 def get_app():
     return app
+
+
+@app.on_event("startup")
+def op_startup():
+    _str = open('config.json').read()
+    cnf = json.loads(_str)
+    server = Server(cnf, cwd="/oidc")
+    app.server = server
+
 
 @app.get("/.well-known/webfinger")
 async def well_known(model: WebFingerRequest = Depends()):
@@ -69,6 +62,3 @@ def verify_user(kwargs: dict, response: Response):
 @app.get('/authorization')
 def authorization(model: AuthorizationRequest = Depends()):
     return service_endpoint(app.server.server_get("endpoint", 'authorization'))
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
